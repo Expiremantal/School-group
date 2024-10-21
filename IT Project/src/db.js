a3cbd1e4f2b1418678e222698e69032215e806b8
Expiremantal <146642155+Expiremantal@users.noreply.github.com>
2024-10-21 19:58:16 +0200

    Delete IT Project/src directory

--- IT Project/src/db.js
+++
@@ -1,8 +0,0 @@
-const { Sequelize } = require('sequelize');
-
-const sequelize = new Sequelize('ShortlistPro_DB', 'root', 'password', {
-  host: 'localhost',
-  dialect: 'mysql',
-});
-
-module.exports = sequelize;
--- IT Project/src/index.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
-import './index.css'; // Import your styles if you have any
-
-const rootElement = document.getElementById('root'); // Ensure this matches your HTML
-const root = ReactDOM.createRoot(rootElement);
-root.render(<App />);
